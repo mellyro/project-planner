d0f685e09e0c91dd87666cbbaf2296f2e5b326aa
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-02-28 15:58:50 -0600

    add dummy project data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'help me find jake', content: 'find where jake is hiding'},
+        {id: '2', title: 'find dog toys', content: 'walk the house and search for all dog toys then put them away'},
+        {id: '3', title: 'walk dogs', content: 'walk the dogs around the park'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
