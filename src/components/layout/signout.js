9deda3c0151219c3baa3a734fb2bffadeca90607
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-02-28 14:38:51 -0600

    fix links and styling

--- src/components/layout/signout.js
+++ src/components/layout/signout.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const SignedOut = () => {
     return (
         <ul className="right">
-            <li><NavLink to='/'>Sign Up</NavLink></li>
-            <li><NavLink to='/'>Log In</NavLink></li>
+            <li><NavLink to='/signup'>Sign Up</NavLink></li>
+            <li><NavLink to='/signin'>Log In</NavLink></li>
         </ul>
     )
 }
