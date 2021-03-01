90c70b5817bd5365eba7ae763340e5e2f0ab4fb7
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-02-28 21:14:46 -0600

    sync data with firestore

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Notifications from './notifications';
 import ProjectList from '../projects/list';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 class Dashboard extends Component {
     render() {
@@ -25,8 +27,13 @@ class Dashboard extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        projects: state.project.projects
+        projects: state.firestore.ordered.projects
     }
 }
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(Dashboard);
