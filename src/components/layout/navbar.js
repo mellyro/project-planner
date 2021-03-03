38544d53ab710ecb23829bc73f2f22e340a9b13f
Melissa Daskalantonakis <melissa.daska@gmail.com>
2021-03-02 19:49:05 -0600

    add firebase auth

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedIn from './signin';
 import SignedOut from './signout';
+import { connect } from 'react-redux';
 
 const Navbar = () => {
     return (
@@ -15,4 +16,10 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps)(Navbar);
